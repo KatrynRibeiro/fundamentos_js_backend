092e13dccafe9e49a6444e9d6b5214ac08fa600c
KatrynRibeiro <kakarilber@hotmail.com>
2022-05-19 13:25:14 -0300

    exemplosjsback1

---
+++ type_bolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5; 
+const segundoNumero = 5;
+// a variável 'const' não pode ser alterado o seu valor no futuro. 
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura"; 
+
+console.log(texto1 === texto2);
---
+++ type_string.js
@@ -0,0 +1,16 @@
+
+const texto1 = "Olá, mundo!";
+const senha = "superSegura123!";
+const StringNumeros = "34576";
+const citacao = "Meu nome é ";
+const meuNome = "Katryn";
+
+// concatenação (+) 
+console.log(citacao + meuNome);
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase(); // Ele está dizendo que a variável input vai ser minúscula. Essa propriedade .toLowerCase) deixa as letras em minusculo. 
+// Esse sinal de (===) está comparando as variáveis e sempre dá V ou F. 
+console.log(cidade === inputMinusculo); // true
